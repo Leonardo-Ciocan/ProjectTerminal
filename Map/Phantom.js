a161d748f91362e7af3a58a826f46329872af231
Maria Vrabie <maria.vrabie@kcl.ac.uk>
2016-03-12 20:24:16 +0000

    Phantom working

---
+++ Map/Phantom.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Maria on 3/12/2016.
+ */
+
+var url = "http://www.google.com";
+
+//var page = require('webpage').create();
+//page.open(url, function(status) {
+//    if(status !== 'success'){
+//        console.log("Unable to load the adress");
+//        phantom.exit(1);
+//    }
+//    else {
+//        setTimeout(function() {
+//            page.render('map.png');
+//            phantom.exit();}, 5000)
+//    }
+//});
+
+
+var phantom = require('phantom');
+
+phantom.create().then(function(ph) {
+    ph.createPage().then(function(page) {
+        page.open(url).then(function(status) {
+            console.log(status);
+            page.property('content').then(function(content) {
+                console.log(content);
+                page.render('map.png');
+                page.close();
+                ph.exit();
+            });
+        });
+    });
+});
