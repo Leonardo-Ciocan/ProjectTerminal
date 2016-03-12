2895c3af6265c1f4c32a758ec6624e6705da2f84
Tom <thomas.owers@kcl.ac.uk>
2016-03-12 20:07:18 +0000

    Added the instagram ruby script

---
+++ Instagram/insta.rb
@@ -0,0 +1,29 @@
+require 'instagram'
+require 'geocoder'
+
+Instagram.configure do |config|
+  config.client_id = "d1a2df2db2364e2bb3ad1bdc8a6c297f"
+  config.client_secret = "b2f2c21a4e0d46108e7ed59f5bcb939f"
+end
+
+locations = [];
+
+insta = Instagram.client(:access_token => "1455523771.d1a2df2.1fc5f49ad9ca44d18ceb5b6a8b08f3f2")
+
+for item in insta.tag_recent_media("hackathon")
+	if item[:location] != nil then
+		a = Geocoder.search("#{item[:location][:latitude]},#{item[:location][:longitude]}").first
+		item[:location].merge!("country" => a.country, "image" => item[:images][:standard_resolution][:url])
+		locations.push(item[:location])
+	end
+end
+
+result = { "type" => "list", "schema" => [ 
+	{ "label" => "latitude", "type" => "decimal"}, 
+	{ "label" => "name", "type" => "decimal"},
+	{ "label" => "id", "type" => "integer"},
+	{ "label" => "country", "type" => "string"},
+	{ "label" => "image", "type" => "image"},
+], "data" => locations }
+
+puts result.to_json
